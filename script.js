6c8f1d638a72a83500f6851a68065cfcaa198145
qabil98 <qedimaliyev1998@gmail.com>
2024-05-02 01:55:15 +0400

    completed

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var number1 = prompt("Birinci ədədi daxil edin:");
+var number2 = prompt("İkinci ədədi daxil edin:");
+
+
+var result = parseFloat(number1) + parseFloat(number2);
+
+
+alert(result);
+
+
+
+
+
+
+
